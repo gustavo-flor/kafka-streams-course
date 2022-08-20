604e5171985cf3fa1c9f3107192b8f156148b69b
Gustavo Flôr <ogustaflor@gmail.com>
2022-08-20 12:44:50 -0300

    :zap: Adding Favourite Color Application Stream

--- README.md
+++ README.md
@@ -1,38 +1,104 @@
 # Kafka Streams Course
 
-## Word Count
+Repository to put in practice concepts of Kafka Streams Course
+
+---
 
 Up Kafka environment
+
 ```shell
 docker-compose up -d
 ```
 
 Access your kafka broker
+
 ```shell
 docker ps # Find the kafka container ID
 
 docker exec -it <Kafka Container ID> bash
 ```
 
+## Word Count
+
 Create input topic
+
 ```shell
-kafka-topics --bootstrap-server localhost:9092 --create --topic word-count-input --partitions 3
+kafka-topics --bootstrap-server localhost:9092 \ 
+  --create \ 
+  --topic word-count-input \ 
+  --partitions 3
 ```
 
 Create output topic
+
 ```shell
-kafka-topics --bootstrap-server localhost:9092 --create --topic word-count-output --partitions 3
+kafka-topics --bootstrap-server localhost:9092 \ 
+  --create \ 
+  --topic word-count-output \ 
+  --partitions 3
 ```
 
 Produce data to be streamed
+
 ```shell
 kafka-console-producer --bootstrap-server localhost:9092 --topic word-count-input
 ```
 
 Consume the stream generate
+
 ```shell
 kafka-console-consumer --bootstrap-server localhost:9092 \
   --topic word-count-output \
+  --from-beginning \
+  --formatter kafka.tools.DefaultMessageFormatter \
+  --property print.key=true \
+  --property print.value=true \
+  --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer \
+  --property value.deserializer=org.apache.kafka.common.serialization.LongDeserializer
+```
+
+## Favourite Color
+
+Create input topic
+
+```shell
+kafka-topics --bootstrap-server localhost:9092 \ 
+  --create \ 
+  --topic favourite-color-input \ 
+  --partitions 3
+```
+
+Create intermediary topic
+
+```shell
+kafka-topics --bootstrap-server localhost:9092 \ 
+  --create \ 
+  --topic favourite-color-stream \ 
+  --partitions 3
+  --config cleanup.policy=compact
+```
+
+Create output topic
+
+```shell
+kafka-topics --bootstrap-server localhost:9092 \ 
+  --create \ 
+  --topic favourite-color-output \ 
+  --partitions 3
+  --config cleanup.policy=compact
+```
+
+Produce data to be streamed
+
+```shell
+kafka-console-producer --bootstrap-server localhost:9092 \ --topic favourite-color-input
+```
+
+Consume the stream generate
+
+```shell
+kafka-console-consumer --bootstrap-server localhost:9092 \
+  --topic favourite-color-output \
   --from-beginning \
   --formatter kafka.tools.DefaultMessageFormatter \
   --property print.key=true \
---
+++ favourite-color/src/main/java/com/github/gustavoflor/kafkastreamscourse/favouritecolor/FavouriteColorApplication.java
@@ -0,0 +1,64 @@
+package com.github.gustavoflor.kafkastreamscourse.favouritecolor;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Named;
+import org.apache.kafka.streams.kstream.Produced;
+
+import java.util.List;
+import java.util.Properties;
+import java.util.UUID;
+import java.util.regex.Pattern;
+
+import static org.apache.kafka.clients.consumer.ConsumerConfig.AUTO_OFFSET_RESET_CONFIG;
+import static org.apache.kafka.streams.StreamsConfig.*;
+import static org.apache.kafka.streams.StreamsConfig.APPLICATION_ID_CONFIG;
+
+public class FavouriteColorApplication {
+    private static final Properties PROPERTIES;
+    private static final String INPUT_TOPIC = "favourite-color-input";
+    private static final String INTERMEDIARY_TOPIC = "favourite-color-stream";
+    private static final String OUTPUT_TOPIC = "favourite-color-output";
+    private static final String COMMA = ",";
+    private static final Pattern TEMPLATE_PATTERN = Pattern.compile("\\w+,\\w+");
+    private static final List<String> AVAILABLE_COLORS = List.of("red", "green", "blue");
+
+    static {
+        PROPERTIES = new Properties();
+        PROPERTIES.setProperty(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        PROPERTIES.setProperty(APPLICATION_ID_CONFIG, "favourite-color-application-1");
+        PROPERTIES.setProperty(DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        PROPERTIES.setProperty(DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        PROPERTIES.setProperty(AUTO_OFFSET_RESET_CONFIG, "earliest");
+        PROPERTIES.setProperty(STATE_DIR_CONFIG, String.format("/tmp/kafka-streams/%s/%s", PROPERTIES.get(APPLICATION_ID_CONFIG), UUID.randomUUID()));
+    }
+
+    public static void main(String[] args) {
+        final StreamsBuilder streamsBuilder = new StreamsBuilder();
+
+        streamsBuilder.stream(INPUT_TOPIC)
+                .mapValues(Object::toString)
+                .filter((key, value) -> TEMPLATE_PATTERN.matcher(value).matches())
+                .selectKey((key, value) -> value.split(COMMA)[0])
+                .mapValues(value -> value.split(COMMA)[1].toLowerCase())
+                .filter((key, value) -> AVAILABLE_COLORS.stream().anyMatch(color -> color.equals(value)))
+                .to(INTERMEDIARY_TOPIC, Produced.with(Serdes.String(), Serdes.String()));
+        streamsBuilder.table(INTERMEDIARY_TOPIC)
+                .groupBy((key, value) -> new KeyValue<>(value.toString(), key))
+                .count(Named.as("FavouriteColorCounter"))
+                .toStream()
+                .to(OUTPUT_TOPIC, Produced.with(Serdes.String(), Serdes.Long()));
+
+        startStreaming(streamsBuilder);
+    }
+
+    private static void startStreaming(final StreamsBuilder streamsBuilder) {
+        final KafkaStreams kafkaStreams = new KafkaStreams(streamsBuilder.build(), PROPERTIES);
+        kafkaStreams.cleanUp(); // ONLY FOR DEVELOPMENT
+        kafkaStreams.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(kafkaStreams::close));
+    }
+}
